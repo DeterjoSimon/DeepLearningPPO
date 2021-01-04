d8a40a587420ff2f70c240a0f9bba651756a8eb7
DeterjoSimon <43176887+DeterjoSimon@users.noreply.github.com>
2021-01-04 19:28:43 +0100

    Add files via upload

---
+++ common/model.py
@@ -0,0 +1,132 @@
+from .misc_util import orthogonal_init
+import torch.nn as nn
+import torch
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.contiguous().view(x.size(0), -1)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self,
+                 in_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        out = nn.ReLU()(x)
+        out = self.conv1(out)
+        out = nn.ReLU()(out)
+        out = self.conv2(out)
+        return out + x
+
+
+class ImpalaBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ImpalaBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.res1 = ResidualBlock(out_channels)
+        self.res2 = ResidualBlock(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)(x)
+        x = self.res1(x)
+        x = self.res2(x)
+        return x
+
+
+class ImpalaModel(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 **kwargs):
+        super(ImpalaModel, self).__init__()
+        self.block1 = ImpalaBlock(in_channels=in_channels, out_channels=16)
+        self.block2 = ImpalaBlock(in_channels=16, out_channels=32)
+        self.block3 = ImpalaBlock(in_channels=32, out_channels=32)
+        self.fc = nn.Linear(in_features=32 * 8 * 8, out_features=256)
+
+        self.output_dim = 256
+        self.apply(orthogonal_init())
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = nn.ReLU()(x)
+        x = Flatten()(x)
+        x = self.fc(x)
+        x = nn.ReLU()(x)
+        return x
+
+
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(GRU, self).__init__()
+        self.gru = orthogonal_init(nn.GRU(input_size, hidden_size), gain=1.0)
+
+    def forward(self, x, hxs, masks):
+        # Prediction
+        if x.size(0) == hxs.size(0):
+            # input for GRU-CELL: (L=sequence_length, N, H)
+            # output for GRU-CELL: (output: (L, N, H), hidden: (L, N, H))
+            masks = masks.unsqueeze(-1)
+            x, hxs = self.gru(x.unsqueeze(0), (hxs * masks).unsqueeze(0))
+            x = x.squeeze(0)
+            hxs = hxs.squeeze(0)
+        # Training
+        # We will recompute the hidden state to allow gradient to be back-propagated through time
+        else:
+            # x is a (T, N, -1) tensor that has been flatten to (T * N, -1)
+            N = hxs.size(0)
+            T = int(x.size(0) / N)
+
+            # unflatten
+            x = x.view(T, N, x.size(1))
+
+            # Same deal with masks
+            masks = masks.view(T, N)
+
+            # Let's figure out which steps in the sequence have a zero for any agent
+            # We will always assume t=0 has a zero in it as that makes the logic cleaner
+            # (can be interpreted as a truncated back-propagation through time)
+            has_zeros = ((masks[1:] == 0.0) \
+                         .any(dim=-1)
+                         .nonzero()
+                         .squeeze()
+                         .cpu())
+
+            # +1 to correct the masks[1:]
+            if has_zeros.dim() == 0:
+                # Deal with scalar
+                has_zeros = [has_zeros.item() + 1]
+            else:
+                has_zeros = (has_zeros + 1).numpy().tolist()
+
+            # add t=0 and t=T to the list
+            has_zeros = [0] + has_zeros + [T]
+
+            hxs = hxs.unsqueeze(0)
+            outputs = []
+            for i in range(len(has_zeros) - 1):
+                # We can now process steps that don't have any zeros in masks together!
+                # This is much faster
+                start_idx = has_zeros[i]
+                end_idx = has_zeros[i + 1]
+
+                rnn_scores, hxs = self.gru(
+                    x[start_idx:end_idx],
+                    hxs * masks[start_idx].view(1, -1, 1))
+
+                outputs.append(rnn_scores)
+
+            # assert len(outputs) == T
+            # x is a (T, N, -1) tensor
+            x = torch.cat(outputs, dim=0)
+            # flatten
+            x = x.view(T * N, -1)
+            hxs = hxs.squeeze(0)
+
+        return x, hxs
---
+++ common/policy.py
@@ -0,0 +1,37 @@
+from .misc_util import orthogonal_init
+from .model import GRU
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Categorical, Normal
+
+class CategoricalPolicy(nn.Module):
+    def __init__(self, 
+                 embedder,
+                 recurrent,
+                 action_size):
+        """
+        embedder: (torch.Tensor) model to extract the embedding for observation
+        action_size: number of the categorical actions
+        """ 
+        super(CategoricalPolicy, self).__init__()
+        self.embedder = embedder
+        # small scale weight-initialization in policy enhances the stability        
+        self.fc_policy = orthogonal_init(nn.Linear(self.embedder.output_dim, action_size), gain=0.01)
+        self.fc_value = orthogonal_init(nn.Linear(self.embedder.output_dim, 1), gain=1.0)
+
+        self.recurrent = recurrent
+        if self.recurrent:
+            self.gru = GRU(self.embedder.output_dim, self.embedder.output_dim)
+
+    def is_recurrent(self):
+        return self.recurrent
+
+    def forward(self, x, hx, masks):
+        hidden = self.embedder(x)
+        if self.recurrent:
+            hidden, hx = self.gru(hidden, hx, masks)
+        logits = self.fc_policy(hidden)
+        log_probs = F.log_softmax(logits, dim=1)
+        p = Categorical(logits=log_probs)
+        v = self.fc_value(hidden).reshape(-1)
+        return p, v, hx
